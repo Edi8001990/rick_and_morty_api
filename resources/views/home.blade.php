88a1f49b6b452170215c4c81d5ed450073f58ab4
Mateusz T <mateusz.trajdos@gmail.com>
2023-07-11 17:12:44 +0100

    Fixed styling for pagination by using 'pagination::bootstrap-4' property at home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -19,7 +19,7 @@
         </div>
 
         <div class="d-flex justify-content-center mt-4">
-            {{ $characters->links() }}
+            {{ $characters->links('pagination::bootstrap-4') }}
         </div>
       </div>
     <script src="{{ asset('js/app.js') }}"></script>
